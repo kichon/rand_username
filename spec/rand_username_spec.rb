18c6c6a818c60dd7f8fac5f2bd91d96742c5f92b
kichon <kikuchi.yoichi@gmail.com>
2020-12-08 12:21:47 +0900

    Fix: テストの追加

--- spec/rand_username_spec.rb
+++ spec/rand_username_spec.rb
@@ -3,7 +3,18 @@ RSpec.describe RandUsername do
     expect(RandUsername::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "nを指定しない場合は6文字" do
+    expect(RandUsername.generate.length).to eq(6)
+  end
+
+  it "nが10の場合は10文字" do
+    expect(RandUsername.generate(n: 10).length).to eq(10)
+  end
+
+  it "nが0の場合はエラー" do
+    expect { RandUsername.generate(n: 0) }.to raise_error(RandUsername::Error)
   end
 end
+
+
+

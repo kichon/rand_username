55a130b36fc2ebd3bcb7cf0bf45349961ecd167f
kichon <kikuchi.yoichi@gmail.com>
2020-12-08 12:21:15 +0900

    Fix: エラー修正と0文字以下の場合は例外になるように修正

--- lib/rand_username.rb
+++ lib/rand_username.rb
@@ -4,7 +4,9 @@ module RandUsername
   class Error < StandardError; end
 
   def self.generate(n: 6)
+    raise Error, "nは0より大きい数を指定してください" if n <= 0
+
     arr = [*('a'..'z'), *('A'..'Z')]
-    n.map { arr[rand(arr.length)] }.join
+    (0...n).map { arr[rand(arr.length)] }.join
   end
 end
